d47e1498cd9b19c672658a25392584b93ab20e4e
Dan <danielcampbell339@gmail.com>
2024-07-12 14:49:24 +0100

    todo list

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MLP To-Do</title>
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row mt-3">
+            <div class="col">
+                <img src="{{ asset('images/logo.png') }}" class="img-fluid" alt="logo">
+            </div>
+        </div>
+        {{ $slot }}
+    </div>
+</body>
+
+<style>
+    body {
+        background-color: #F1F1F1;
+    }
+</style>
+
+</html>
--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,16 +1,68 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>MLP To-Do</title>
+<x-layout>
+    <div class="row mt-5">
+        <div class="col-4">
+            <x-form action="{{ route('task.create') }}" method="post">
+                <div class="input-group mb-3">
+                    <input name="name" type="text" class="form-control" placeholder="Insert task name">
+                </div>
+                <div class="input-group mb-3">
+                    <button type="submit" class="btn btn-primary btn-block add-task">Add</button>
+                </div>
+            </x-form>
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        </div>
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <table class="table table-sm">
+                        <th scope="col" style="width:5%">#</th>
+                        <th scope="col">Task</th>
+                        <th scope="col"></th>
+                        <tbody>
+                            @foreach ($tasks as $key => $task)
+                                <tr>
+                                    <td>{{ $key + 1 }}</td>
+                                    <td class={{ $task->is_done ? 'task_done' : '' }}>
+                                        {{ $task->name }}
+                                    </td>
+                                    <td>
+                                        @if (!$task->is_done)
+                                            <x-form action="{{ route('task.delete', $task) }}" method="delete">
+                                                <button class="btn btn-danger ml-2 btn-sm float-right">
+                                                    <i class="fa-solid fa-xmark"></i>
+                                                </button>
+                                            </x-form>
+                                            <x-form action="{{ route('task.done', $task) }}" method="post">
+                                                <button class="btn btn-success btn-sm float-right">
+                                                    <i class="fa-solid fa-check"></i>
+                                                </button>
+                                            </x-form>
+                                        @endif
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+<style>
+    .task_done {
+        text-decoration: line-through;
+    }
 
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
-</head>
-<body>
-
-</body>
-</html>
+    .add-task {
+        background-color: #337AB7;
+    }
+</style>
